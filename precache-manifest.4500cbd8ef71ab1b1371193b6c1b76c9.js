a7c139eb10cee1f183679ff0968eebeb957a7d8e
Pawan bisht <pawanbisht83@gmail.com>
2020-04-15 19:30:40 +0530

    Updates

---
+++ precache-manifest.4500cbd8ef71ab1b1371193b6c1b76c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f238a3b8cc287fe0eb0a39c3c22b6400",
+    "url": "/rolodex-monsters-reactjs/index.html"
+  },
+  {
+    "revision": "f04b53189a0e3b0cf927",
+    "url": "/rolodex-monsters-reactjs/static/css/main.dd902f86.chunk.css"
+  },
+  {
+    "revision": "4351a4309c453092483e",
+    "url": "/rolodex-monsters-reactjs/static/js/2.17e1d4dc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rolodex-monsters-reactjs/static/js/2.17e1d4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04b53189a0e3b0cf927",
+    "url": "/rolodex-monsters-reactjs/static/js/main.3b6c689f.chunk.js"
+  },
+  {
+    "revision": "839a7309a2364a1c3007",
+    "url": "/rolodex-monsters-reactjs/static/js/runtime-main.946cbc24.js"
+  }
+]);
